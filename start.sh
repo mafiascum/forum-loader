35dfd61e18f72dd4d402515a651baba4d9b3c78e
Unknown <ccatlett2000@mctherealm.net>
2017-09-14 23:45:53 -0400

    Prevent container crash when /mnt/dev doesn't exist

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 find /var/www/html/ext/ -mindepth 1 -maxdepth 1 -type l -delete
-find /mnt/dev/ -maxdepth 1 -mindepth 1 -type d -exec ln -s -t /var/www/html/ext/ {} \;
+# Check two layers down so that it works even when /mnt/dev doesn't exist
+find /mnt/ -maxdepth 2 -mindepth 2 -type d -exec ln -s -t /var/www/html/ext/ {} \;
 
 # Passthrough to php:apache start
 apache2-foreground
