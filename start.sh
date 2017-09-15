06d8ce6c949347f4245d9f05f9d36d739d27d5d3
Unknown <ccatlett2000@mctherealm.net>
2017-09-15 00:18:05 -0400

    Overwrite existing addons when loading dev/

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-find /var/www/html/ext/ -mindepth 1 -maxdepth 1 -type l -delete
+mkdir -p /var/www/html/ext/mafiascum/
+find /var/www/html/ext/mafiascum/ -mindepth 1 -maxdepth 1 -type l -delete
 # Check two layers down so that it works even when /mnt/dev doesn't exist
-find /mnt/ -maxdepth 2 -mindepth 2 -type d -exec ln -s -t /var/www/html/ext/ {} \;
+find /mnt/ -maxdepth 2 -mindepth 2 -type d -exec basename {} \; | xargs -I {} rm -rf /var/www/html/ext/mafiascum/{}
+find /mnt/ -mindepth 2 -maxdepth 2 -type d -exec ln -s -t /var/www/html/ext/mafiascum/ {} \;
 
 # Passthrough to php:apache start
 apache2-foreground
