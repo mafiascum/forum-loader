c8351865a8ac4e9cd7fdd169089527b0e459a01d
Charlie Ciccia <charlie.ciccia@singlewire.com>
2018-03-01 13:37:05 -0600

    parametrize branch name

--- install_git_extensions.sh
+++ install_git_extensions.sh
@@ -2,6 +2,6 @@
 mkdir -p /var/www/html/ext/mafiascum/
 cd /var/www/html/ext/mafiascum/
 cat /tmp/git_extensions | while read repo; do
-  git clone --depth=1 --branch=master $repo
+  git clone --depth=1 --branch=${BRANCH_NAME-master} $repo
 done
 rm -rf /var/www/html/ext/mafiascum/*/.git/
--- install_git_styles.sh
+++ install_git_styles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/www/html/styles/
 cat /tmp/git_styles | while read repo; do
-  git clone --depth=1 --branch=master $repo
+  git clone --depth=1 --branch=${BRANCH_NAME-master} $repo
 done
 rm -rf /var/www/html/styles/*/.git/
