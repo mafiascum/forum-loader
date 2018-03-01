8cf3ac9d9fa5e3f9cdbfec64e9662900f74bd270
Charlie Ciccia <charlie.ciccia@singlewire.com>
2018-03-01 14:30:55 -0600

    we want ARG, not ENV here.

--- install_git_extensions.sh
+++ install_git_extensions.sh
@@ -3,6 +3,6 @@ mkdir -p /var/www/html/ext/mafiascum/
 cd /var/www/html/ext/mafiascum/
 echo "using branch ${BRANCH_NAME-master}"
 cat /tmp/git_extensions | while read repo; do
-  git clone --depth=1 --branch=${BRANCH_NAME-master} $repo
+  git clone --depth=1 --branch=${BRANCH_NAME:-master} $repo
 done
 rm -rf /var/www/html/ext/mafiascum/*/.git/
